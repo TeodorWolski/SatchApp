5e64987452e6c5687234d311e4a29806af661da4
Teodor Wolski <teodorwolski88@gmail.com>
2021-04-04 14:12:57 +0200

    fixing redux bug

--- satchapp/src/redux/reducers/index.js
+++ satchapp/src/redux/reducers/index.js
@@ -1,50 +1,11 @@
 const initialState = {
-  saves: [
-    {
-      id: 1,
-      title: 'Hello meow',
-      created: '18 may 2018',
-      link: 'https://www.youtube.com/watch?v=-QmyosHh-kU',
-      content: 'Nowy film gargamela!',
-    },
-    {
-      id: 2,
-      title: 'Hello meow',
-      created: '18 may 2018',
-      link: 'https://www.youtube.com/watch?v=-QmyosHa-kU',
-      content: 'Nowy film!',
-    },
-    {
-      id: 3,
-      title: 'Hello meow',
-      created: '18 may 2018',
-      link: 'https://www.youtube.com/watch?v=-QmyosHy-kU',
-      content: 'Nowy film gorgonzoli!',
-    },
-    {
-      id: 4,
-      title: 'Hello meow',
-      created: '18 may 2018',
-      link: 'https://www.youtube.com/watch?v=-QmyosHy-kU',
-      content: 'Nowy film gorgonzoli!',
-    },
-    {
-      id: 5,
-      title: 'Hello meow',
-      created: '18 may 2018',
-      link: 'https://www.youtube.com/watch?v=-QmyosHy-kU',
-      content: 'Nowy film gorgonzoli!',
-    },
-  ],
+  saves: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
-      return {
-        ...state,
-        state: [...state.saves, action.payload],
-      };
+      return { ...state, saves: [...state.saves, action.payload] };
     case 'REMOVE_ITEM':
       return {
         ...state,
@@ -54,9 +15,5 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
-
-// [action.payload.savedVideos]: [
-//   ...state[action.payload.savedVideos].filter((item) => item.id !== action.payload.id),
-// ],
 
 export default reducer;
